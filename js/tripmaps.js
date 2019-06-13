c4b2a8d7dcbc1add4f0bf223add35336d2a198ea
Ingo Scholtes <ischoltes@ethz.ch>
2019-06-13 19:56:52 +0200

    switching to OSM

--- js/tripmaps.js
+++ js/tripmaps.js
@@ -8,52 +8,50 @@
 
 // uses Here.com JS 3.0
 
-function switchMapType(map, platform) {
-	"use strict";	
- 	var ts = platform.getMapTileService({ type: 'satellite' });
-  	var terrainMap = ts.createTileLayer(
-		'maptile',
-		'satellite.day',
-		pixelRatio === 1 ? 256 : 512,
-		'png8',
-		{ppi: pixelRatio === 1 ? undefined : 320}
-	);
-  	map.setBaseLayer(terrainMap);
-}
-
 // Shows a map centered at the given location and zoom, and adds a kml path 
 function showMap(lat, lng, zoom, kml, divid) {
-	"use strict";
 	
-	var div = document.getElementById(divid);
-	
-	var platform = new H.service.Platform({
-		app_id: 'WxZYu1p8L4ClnkebR416', 
-		app_code: 'DNvkAh6zMfL88GCGrhNqWA',
-		useHTTPS: true
-	});
-	
-	var defaultLayers = platform.createDefaultLayers();	
-	
-	var map = new H.Map(div, defaultLayers.satellite.map);
-		
-	// Parse track data
-	var reader = new H.data.kml.Reader(kml);
+	var map = new OpenLayers.Map (divid, {
+		controls:[
+			new OpenLayers.Control.Navigation(),
+			new OpenLayers.Control.PanZoomBar(),
+			new OpenLayers.Control.LayerSwitcher(),
+			new OpenLayers.Control.Attribution()],
+		maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
+		maxResolution: 156543.0399,
+		numZoomLevels: 19,
+		units: 'm',
+		projection: new OpenLayers.Projection("EPSG:900913"),
+		displayProjection: new OpenLayers.Projection("EPSG:4326")
+	} );
 
-	reader.parse();
+	// Define the map layer
+	// Here we use a predefined layer that will be kept up to date with URL changes
+	layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+	map.addLayer(layerMapnik);
+	layerCycleMap = new OpenLayers.Layer.OSM.CycleMap("CycleMap");
+	map.addLayer(layerCycleMap);
+	layerMarkers = new OpenLayers.Layer.Markers("Markers");
+	map.addLayer(layerMarkers);
 
-	reader.addEventListener('statechange', function () {
-		// Wait till the KML document is fully loaded and parsed
-		if (this.getState() === H.data.AbstractReader.State.READY) {
-		  var parsedObjects = reader.getParsedObjects();
-		  // Create a group from our objects to easily zoom to them
-		  var container = new H.map.Group({objects: parsedObjects});
-		  map.setViewBounds(parsedObjects[0].getBounds());
-		  map.addObject(container)
-		}
+	// Add the Layer with the GPX Track
+	var lgpx = new OpenLayers.Layer.Vector("Lakeside cycle ride", {
+		strategies: [new OpenLayers.Strategy.Fixed()],
+		protocol: new OpenLayers.Protocol.HTTP({
+			url: kml,
+			format: new OpenLayers.Format.KML()
+		}),
+		style: {strokeColor: "red", strokeWidth: 5, strokeOpacity: 0.5},
+		projection: new OpenLayers.Projection("EPSG:4326")
 	});
-	
-	// MapEvents enables the event system
-	// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-	var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+	map.addLayer(lgpx);
+
+	// Add a Layer with Marker
+	var size = new OpenLayers.Size(21, 25);
+	var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+	var icon = new OpenLayers.Icon('https://www.openstreetmap.org/openlayers/img/marker.png',size,offset);
+	layerMarkers.addMarker(new OpenLayers.Marker(lonLat,icon));
+
+	var lonLat = new OpenLayers.LonLat(lng, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+	map.setCenter(lonLat, zoom);	
 }
