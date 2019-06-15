4346b90a7cb33af637dfa1200879e06692b821a9
Ingo Scholtes <ischoltes@ethz.ch>
2019-06-15 16:24:28 +0200

    -

--- js/tripmaps.js
+++ js/tripmaps.js
@@ -1,22 +1,16 @@
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
-import TileLayer from 'ol/layer/Tile.js';
-import OSM from 'ol/source/OSM.js';
-import KML from 'ol/format/KML.js';
-import VectorSource from 'ol/source/Vector.js';
-import ZoomToExtent from 'ol/control/ZoomToExtent';
+import ol from 'ol.js'
 
 // Shows a map centered at the given location and zoom, and adds a kml path
 function showMap(lat, lng, zoom, kml, divid) {
 	
-	var map = new Map({
+	var map = new ol.Map({
 		layers: [
 			new TileLayer({
 				source: new OSM()
 			})
 		],
 		target: divid,
-		view: new View({
+		view: new ol.View({
 			center: [0, 0],
 			zoom: 2
 		})
