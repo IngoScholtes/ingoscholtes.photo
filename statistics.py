4990d94dfb505e2a88d535599aba21778b231f9a
Ingo Scholtes <scholtes@uni-wuppertal.de>
2021-10-03 21:28:02 +0200

    new category pages

--- statistics.py
+++ statistics.py
@@ -35,3 +35,99 @@ print(alt, ' m')
 
 
 #%%
+
+def country_stats(country):
+    print(' '.join(country))
+    num = 0
+    dist = 0
+    ascend = 0
+    time_h = 0
+    time_m = 0
+
+    for x in hikes.keys():    
+        if hikes[x]['country'] in country:
+            num += 1
+            dist += hikes[x]['distance']
+            ascend += hikes[x]['ascend']
+            time_h += hikes[x]['time_h']
+            time_m += hikes[x]['time_m']
+    print(num, ' tours')
+    print(dist, ' km')
+    print(ascend, ' m')
+
+    time_h += time_m//60
+    time_m = time_m%60
+
+    print(time_h, ' hours')
+    print(time_m, ' minutes')
+    print('----')
+
+def canoe_stats():
+    print('Canoe')
+    num = 0
+    dist = 0
+    ascend = 0
+    time_h = 0
+    time_m = 0
+
+    for x in canoe.keys():    
+        num += 1
+        dist += canoe[x]['distance']
+        time_h += canoe[x]['time_h']
+        time_m += canoe[x]['time_m']
+    print(num, ' tours')
+    print(dist, ' km')
+
+    time_h += time_m//60
+    time_m = time_m%60
+
+    print(time_h, ' hours')
+    print(time_m, ' minutes')
+    print('----')
+
+def region_stats(region):
+    print(' '.join(region))
+    num = 0
+    dist = 0
+    ascend = 0
+    time_h = 0
+    time_m = 0
+
+    for x in hikes.keys():    
+        if hikes[x]['region'] in region:
+            num += 1
+            dist += hikes[x]['distance']
+            ascend += hikes[x]['ascend']
+            time_h += hikes[x]['time_h']
+            time_m += hikes[x]['time_m']
+    print(num, ' tours')
+    print(dist, ' km')
+    print(ascend, ' m')
+
+    time_h += time_m//60
+    time_m = time_m%60
+
+    print(time_h, ' hours')
+    print(time_m, ' minutes')
+    print('----')
+
+region_stats(['Hunsrück', 'Hunsr&uuml;ck', 'Eifel', 'Pfalz'])
+
+region_stats(['Black Forest'])
+
+region_stats(['Madeira'])
+
+country_stats(['Switzerland'])
+
+country_stats(['Sweden', 'Norway', 'Denmark'])
+
+country_stats(['USA', 'Canada'])
+
+country_stats(['France', 'Italy', 'Spain'])
+
+canoe_stats()
+    
+# hunsrueck = hikes.unstack()
+# print(hunsrueck[hunsrueck['region']=='Hunsrück'])
+# print('Hunsrueck', hunsrueck.loc['distance'].sum())
+# %%
