6eba677df78d0ae7c8dcae9b7c88503ea2a0b323
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2023-08-22 02:04:12 +0200

    -

--- statistics.py
+++ statistics.py
@@ -7,11 +7,11 @@ hikes = pd.read_json('outdoors/hike_db.json')
 canoe = pd.read_json('outdoors/canoe_db.json')
 
 #%%
-dist = 653 + 855 + 35 + 140 + 123 + 33 + 123 + 20 + 37
-alt = 6500 + 10988 + 1036 + 4585 + 3608 + 1017 + 4338 + 1468 + 1223
-time_h = 46 + 8 + 34 + 29 + 9 + 32 + 6 + 10
-time_m = 7 + 38 + 43 + 32 + 39 + 31 + 3 + 5
-num = 9
+dist = 653 + 855 + 35 + 140 + 123 + 33 + 123 + 20 + 37 + 35
+alt = 6500 + 10988 + 1036 + 4585 + 3608 + 1017 + 4338 + 1468 + 1223 + 1530
+time_h = 46 + 8 + 34 + 29 + 9 + 32 + 6 + 10 + 9
+time_m = 7 + 38 + 43 + 32 + 39 + 31 + 3 + 5 + 40
+num = 10
 
 dist += hikes.loc['distance'].sum()
 alt += hikes.loc['ascend'].sum()
